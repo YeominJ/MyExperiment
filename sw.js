36f399d9ecf59e3c057d1ff8e98b874dd3759aa1
Yeomin Jeong <ymjeong@isslab.korea.ac.kr>
2022-03-15 17:49:45 +0900

    2022-03-15

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-if (navigator.serviceWorker){
+// if (navigator.serviceWorker){
 
-    // postMessage로 받은 데이터 콘솔에 출력
-    navigator.serviceWorker.addEventListener('message', event => {      
-        console.print(event.data);                            
-    });
+//     // postMessage로 받은 데이터 콘솔에 출력
+//     navigator.serviceWorker.addEventListener('message', event => {      
+//         console.print(event.data);                            
+//     });
 
-}
+// }
 
 self.addEventListener("message", function (event){
     // const{
