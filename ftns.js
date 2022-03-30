c44f39b1f03da7bb64752a106b09b02afc879968
Yeomin Jeong <ymjeong@isslab.korea.ac.kr>
2022-03-30 18:13:52 +0900

    2022-03-30

---
+++ ftns.js
@@ -0,0 +1,37 @@
+var childwin;
+const childname = "popup";
+
+function openChild() {
+    childwin = window.open('page2.html', childname, 'height=300px, width=500px');
+}
+
+function sendMessage() {
+    let msg = {
+        pName: "Bob",
+        pAge: "35"
+    };
+    // childwin.postMessage(msg);
+    // childwin.focus();
+}
+
+function send2SW() {
+    let msg = {
+        command: "print",
+        sentence: "Hello Service Worker!",
+        url: '"https://sistinalove.github.io/SWtesting/sw.js"',
+        url2: '"https://web-sdk.urbanairship.com/notify/v1/ua-sdk.min.js"',
+        url3: "sample.js",
+        type: "push",
+        push: "https://www.google.com",
+        action : "preview",
+        payload : JSON.stringfy({                
+            title : "제발 되면 좋겠다",
+            body : "오늘 안에는 해결이 될까?"
+        })                 
+    };
+
+    // 서비스 워커가 있으면 DOM -> SW로 MSG 보내기
+    if (navigator.serviceWorker.controller) {
+        navigator.serviceWorker.controller.postMessage(msg);
+    }
+}
