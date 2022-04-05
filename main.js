a86259d398100193e1da3de86b00b5594b840a5c
Yeomin Jeong <ymjeong@isslab.korea.ac.kr>
2022-04-05 13:31:40 +0900

    2022-04-05

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ navigator.serviceWorker.register("./test.js").then(function(){
     console.log("Service Worker Registered!");
 });
 
-var button = document.getElementById("notifications");
+// var button = document.getElementById("notifications");
 
-button.addEventListener("click", function(event){
-    Notification.requestPermission().then(function(result){
-        if(result === 'granted'){
-            // randomNotification();
-            console.log("Permission OK");
-        }
-    });
-});
+// button.addEventListener("click", function(event){
+//     Notification.requestPermission().then(function(result){
+//         if(result === 'granted'){
+//             // randomNotification();
+//             console.log("Permission OK");
+//         }
+//     });
+// });
