f8cee11d508120a67af5dadba7fa88f25f4971d6
Yeomin Jeong <ymjeong@isslab.korea.ac.kr>
2022-04-05 11:28:15 +0900

    2022-04-05

--- ftns.js
+++ ftns.js
@@ -66,6 +66,10 @@ function send2SW() {
 
     // 서비스 워커가 있으면 DOM -> SW로 MSG 보내기
     if (navigator.serviceWorker.controller) {
+        console.log("********FETCH******");
         navigator.serviceWorker.controller.postMessage(testMsg2);
+        console.log("********PUSH*******");
+        navigator.serviceWorker.controller.postMessage(testMsg);
+
     }
 }
