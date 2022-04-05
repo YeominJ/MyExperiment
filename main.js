9f27a09beea73557a48b1c62391acf91bb825884
Yeomin Jeong <ymjeong@isslab.korea.ac.kr>
2022-04-05 10:37:43 +0900

    2022-04-05

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ navigator.serviceWorker.register("./test.js").then(function(){
     console.log("Service Worker Registered!");
 });
 
+
+self.addEventListener("activate", function(event){
+    console.log("Service Worker is Activating!!");
+    // event Listeer가 이상 없이 작동한다는 것을 확실하게 하기 위해
+    return self.clients.claim();
+});
